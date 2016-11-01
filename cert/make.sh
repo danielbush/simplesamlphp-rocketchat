e5bb86cf512f0b3d6933e7923a426aa9b78728d5
Daniel Bush <danb@catalyst-au.net>
2016-11-01 12:11:35 +1100

    We can create certs.

--- cert/make.sh
+++ cert/make.sh
@@ -1,1 +1,3 @@
-openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout ./saml.pem -out ./saml.crt
+#!/bin/bash
+test -n $1 || { echo "Need name of cert/key ie <name>.{pem,crt}."; exit 1; }
+openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout ./$1.pem -out ./$1.crt
